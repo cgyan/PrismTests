f259060a22aa65469b4c3446ffbecf4c3b7fe680
iainhemstock <iainhemstock@googlemail.com>
2016-12-23 23:13:36 +0000

    added InitialisedWithFiveLotsOfValueOfTen

--- src/Vector/NewVector.cpp
+++ src/Vector/NewVector.cpp
@@ -14,9 +14,25 @@ using namespace ::testing;
 PRISM_BEGIN_NAMESPACE
 PRISM_BEGIN_TEST_NAMESPACE
 
-class NewVector : public Test {
+//=============================================================================
+// NewVector is a fixture that tests the vector's various constructors
+//=============================================================================
+class NewVector : public Test
+{};
 
-};
+TEST_F(NewVector,
+InitialisedWithFiveLotsOfValueOfTen) {
+	int numElements = 5;
+	int value = 10;
+
+	PVector<int> v(numElements, value);
+
+	ASSERT_TRUE(10 == v.at(0));
+	ASSERT_TRUE(10 == v.at(1));
+	ASSERT_TRUE(10 == v.at(2));
+	ASSERT_TRUE(10 == v.at(3));
+	ASSERT_TRUE(10 == v.at(4));
+}
 
 PRISM_END_TEST_NAMESPACE
 PRISM_END_NAMESPACE
