d79eb0db89d492ef7c7ad63071df6e49278efa8e
iainhemstock <iainhemstock@googlemail.com>
2016-12-23 23:02:23 +0000

    created new test fixture

---
+++ src/Vector/NewVector.cpp
@@ -0,0 +1,32 @@
+/*
+ * NewVector.cpp
+ * v0.*
+ *
+ *  Created on: 23 Dec 2016
+ *      Author: iainhemstock
+ */
+
+#include "gtest/gtest.h"
+#include <prism/PVector>
+#include <prism/global>
+using namespace ::testing;
+
+PRISM_BEGIN_NAMESPACE
+PRISM_BEGIN_TEST_NAMESPACE
+
+class NewVector : public Test {
+
+};
+
+PRISM_END_TEST_NAMESPACE
+PRISM_END_NAMESPACE
+
+
+
+
+
+
+
+
+
+
