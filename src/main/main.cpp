36d386a26d021bd1993eabeb5bff93d70edf8c7d
iainhemstock <iainhemstock@googlemail.com>
2018-03-18 12:46:19 +0000

    committing

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,10 +5,10 @@
  *      Author: iainhemstock
  */
 
-#include "gtest/gtest.h"
+#include <gmock/gmock.h>
 using namespace ::testing;
 
 int main(int argc, char * argv[]) {
-	InitGoogleTest(&argc, argv);
+	InitGoogleMock(&argc, argv);
 	return RUN_ALL_TESTS();
 }
