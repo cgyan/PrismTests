494f0633866be02035a83f728b28fa988f052ea5
iainhemstock <iainhemstock@googlemail.com>
2018-03-23 12:12:13 +0000

    committing

--- unit/sources.cmake
+++ unit/sources.cmake
@@ -1,64 +1,59 @@
-set(MAIN_SOURCE ${MAIN_SOURCE}
-    src/main/main.cpp
-)
-
 set(CORE_SOURCES ${CORE_SOURCES}
-    src/core/AlgorithmMoveTests.cpp
-    src/core/IteratorTests.cpp
-    src/core/json/JsonArrayTests.cpp
-    src/core/json/JsonDocTests.cpp
-    src/core/json/JsonLexerTests.cpp
-    src/core/json/JsonObjectIteratorTests.cpp
-    src/core/json/JsonObjectTests.cpp
-    src/core/json/JsonParserExceptionTests.cpp
-    src/core/json/JsonStringBuilderTests.cpp
-    src/core/json/JsonValueTests.cpp
-    src/core/json/fakes/FakeJsonArray.cpp
-    src/core/json/fakes/FakeJsonObject.cpp
-    src/core/json/fakes/FakeJsonValue.cpp
-    src/core/ListTests.cpp
-    src/core/ObjectTests.cpp
-    src/core/PairTests.cpp
-    src/core/PointTests.cpp
-    src/core/PStringTests.cpp
-    src/core/RectTests.cpp
-    src/core/SizeTests.cpp
-    src/core/StackTests.cpp
-    src/core/StringBuilderTests.cpp
-    src/core/TimeTests.cpp
-    src/core/VectorTests.cpp
+    unit/src/core/AlgorithmMoveTests.cpp
+    unit/src/core/IteratorTests.cpp
+    unit/src/core/json/JsonArrayTests.cpp
+    unit/src/core/json/JsonDocTests.cpp
+    unit/src/core/json/JsonLexerTests.cpp
+    unit/src/core/json/JsonObjectIteratorTests.cpp
+    unit/src/core/json/JsonObjectTests.cpp
+    unit/src/core/json/JsonParserExceptionTests.cpp
+    unit/src/core/json/JsonStringBuilderTests.cpp
+    unit/src/core/json/JsonValueTests.cpp
+    unit/src/core/json/fakes/FakeJsonArray.cpp
+    unit/src/core/json/fakes/FakeJsonObject.cpp
+    unit/src/core/json/fakes/FakeJsonValue.cpp
+    unit/src/core/ListTests.cpp
+    unit/src/core/ObjectTests.cpp
+    unit/src/core/PairTests.cpp
+    unit/src/core/PointTests.cpp
+    unit/src/core/PStringTests.cpp
+    unit/src/core/RectTests.cpp
+    unit/src/core/SizeTests.cpp
+    unit/src/core/StackTests.cpp
+    unit/src/core/StringBuilderTests.cpp
+    unit/src/core/TimeTests.cpp
+    unit/src/core/VectorTests.cpp
 )
 
 set(NON_TDD_SOURCES ${NON_TDD_SOURCES}
-    src/non_tdd_unit_tests/algorithmTest.cpp
-    src/non_tdd_unit_tests/ArrayTest.cpp
-    src/non_tdd_unit_tests/BitvectorTest.cpp
-    src/non_tdd_unit_tests/CircleTest.cpp
-    src/non_tdd_unit_tests/DequeTest.cpp
-    src/non_tdd_unit_tests/functorTest.cpp
-    src/non_tdd_unit_tests/ListTest.cpp
-    src/non_tdd_unit_tests/ReverseIteratorTest.cpp
-    src/non_tdd_unit_tests/SharedDataPointerTest.cpp
-    src/non_tdd_unit_tests/SharedDataTest.cpp
-    src/non_tdd_unit_tests/StringTest.cpp
-    src/non_tdd_unit_tests/TimeTest.cpp
-    src/non_tdd_unit_tests/UniquePointerArrayTest.cpp
-    src/non_tdd_unit_tests/UniquePointerTest.cpp
-    src/non_tdd_unit_tests/VersionTest.cpp
+    unit/src/non_tdd_unit_tests/algorithmTest.cpp
+    unit/src/non_tdd_unit_tests/ArrayTest.cpp
+    unit/src/non_tdd_unit_tests/BitvectorTest.cpp
+    unit/src/non_tdd_unit_tests/CircleTest.cpp
+    unit/src/non_tdd_unit_tests/DequeTest.cpp
+    unit/src/non_tdd_unit_tests/functorTest.cpp
+    unit/src/non_tdd_unit_tests/ListTest.cpp
+    unit/src/non_tdd_unit_tests/ReverseIteratorTest.cpp
+    unit/src/non_tdd_unit_tests/SharedDataPointerTest.cpp
+    unit/src/non_tdd_unit_tests/SharedDataTest.cpp
+    unit/src/non_tdd_unit_tests/StringTest.cpp
+    unit/src/non_tdd_unit_tests/TimeTest.cpp
+    unit/src/non_tdd_unit_tests/UniquePointerArrayTest.cpp
+    unit/src/non_tdd_unit_tests/UniquePointerTest.cpp
+    unit/src/non_tdd_unit_tests/VersionTest.cpp
 )
 
 set(EVENT_SOURCES ${EVENT_SOURCES}
-    src/eventsystem/PostedEventQueueTest.cpp
-    src/eventsystem/RunLoopTest.cpp
-    src/eventsystem/Win32EventQueueTest.cpp
+    unit/src/eventsystem/PostedEventQueueTest.cpp
+    unit/src/eventsystem/RunLoopTest.cpp
+    unit/src/eventsystem/Win32EventQueueTest.cpp
 )
 
 set(GUI_SOURCES ${GUI_SOURCES}
-    src/gui/WidgetTests.cpp
+    unit/src/gui/WidgetTests.cpp
 )
 
-set(SOURCES
-    ${MAIN_SOURCE}
+set(UNIT_SOURCES
     ${CORE_SOURCES}
     ${NON_TDD_SOURCES}
     # ${EVENT_SOURCES}
