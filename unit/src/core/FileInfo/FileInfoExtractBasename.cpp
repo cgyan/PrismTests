c28d1c67415ef653c617aeb5de00fd5b6cc5d10b
iainhemstock <iainhemstock@googlemail.com>
2018-03-29 11:09:08 +0100

    add more tests for FileInfo::basename

--- unit/src/core/FileInfo/FileInfoExtractBasename.cpp
+++ unit/src/core/FileInfo/FileInfoExtractBasename.cpp
@@ -6,15 +6,38 @@ using namespace ::testing;
 PRISM_BEGIN_NAMESPACE
 PRISM_BEGIN_TEST_NAMESPACE
 
-class FileInfoExtractBasename : public Test {
+class FileInfoBasename : public Test {
 public:
         FileInfo testSubject;
 };
 
-TEST_F(FileInfoExtractBasename, WhenFilenameContainsBasenameAndSuffixExpectBasenameReturned)
+TEST_F(FileInfoBasename, ShouldReturnBasenameWhenFilenameContainsBasenameAndSuffix)
 {
         const std::string filenameContainingBasenameAndSuffix = "file.txt";
         testSubject.setFile(filenameContainingBasenameAndSuffix);
+        const std::string expected = "file";
+        EXPECT_EQ(expected, testSubject.basename());
+}
+
+TEST_F(FileInfoBasename, ShouldReturnEmptyStringWhenFilenameOnlyContainsSuffix)
+{
+        const std::string filenameContainingSuffix = ".txt";
+        testSubject.setFile(filenameContainingSuffix);
+        EXPECT_EQ("", testSubject.basename());
+}
+
+TEST_F(FileInfoBasename, ShouldReturnBasenameWhenFilenameIsLocatedInThisDirectory)
+{
+        const std::string filenameInThisDirectory = "./file.txt";
+        testSubject.setFile(filenameInThisDirectory);
+        const std::string expected = "file";
+        EXPECT_EQ(expected, testSubject.basename());
+}
+
+TEST_F(FileInfoBasename, ShouldReturnBasenameWhenFilenameIsLocatedInParentDirectory)
+{
+        const std::string filenameInParentDir = "../file.txt";
+        testSubject.setFile(filenameInParentDir);
         const std::string expected = "file";
         EXPECT_EQ(expected, testSubject.basename());
 }
