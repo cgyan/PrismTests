0369f76f9aa1e946e01c6b9837fa7d8032c7255e
iainhemstock <iainhemstock@googlemail.com>
2018-03-29 15:06:20 +0100

    Add more unit tests for FileInfo::basename

--- unit/src/core/FileInfo/FileInfoExtractBasename.cpp
+++ unit/src/core/FileInfo/FileInfoExtractBasename.cpp
@@ -26,7 +26,7 @@ TEST_F(FileInfoBasenameTest, ShouldReturnEmptyStringWhenFilenameOnlyContainsSuff
         EXPECT_EQ("", testSubject.basename());
 }
 
-TEST_F(FileInfoBasenameTest, ShouldReturnBasenameWhenFilenameIsLocatedInThisDirectory)
+TEST_F(FileInfoBasenameTest, ShouldReturnBasenameWhenFilenameIsLocatedInCurrentDirectory)
 {
         const std::string filenameInThisDirectory = "./file.txt";
         testSubject.setFile(filenameInThisDirectory);
@@ -42,7 +42,44 @@ TEST_F(FileInfoBasenameTest, ShouldReturnBasenameWhenFilenameIsLocatedInParentDi
         EXPECT_EQ(expected, testSubject.basename());
 }
 
+TEST_F(FileInfoBasenameTest, ShouldReturnBasenameWhenFilenameIsLocatedInSubdirectory)
+{
+        const std::string filenameInSubdirectory = "files/file.txt";
+        testSubject.setFile(filenameInSubdirectory);
+        const std::string expected = "file";
+        EXPECT_EQ(expected, testSubject.basename());
+}
 
+TEST_F(FileInfoBasenameTest, ShouldReturnBasenameWhenWindowsFilenameIsAbsolute)
+{
+        const std::string filenameAbsolute = "c:\\file.txt";
+        testSubject.setFile(filenameAbsolute);
+        const std::string expected = "file";
+        EXPECT_EQ(expected, testSubject.basename());
+}
+
+TEST_F(FileInfoBasenameTest, ShouldReturnBasenameWhenUnixFilenameIsAbsolute)
+{
+        const std::string filenameAbsolute = "/usr/file.txt";
+        testSubject.setFile(filenameAbsolute);
+        const std::string expected = "file";
+        EXPECT_EQ(expected, testSubject.basename());
+}
+
+TEST_F(FileInfoBasenameTest, ShouldReturnBasenameWhenFilenameOnlyContainsBasename)
+{
+        const std::string filenameWithOnlyBasename = "file";
+        testSubject.setFile(filenameWithOnlyBasename);
+        EXPECT_EQ(filenameWithOnlyBasename, testSubject.basename());
+}
+
+TEST_F(FileInfoBasenameTest, ShouldReturnBasenameWhenFilenameHasMultipleSuffixes)
+{
+        const std::string filenameWithMultipleSuffixes = "file.tar.gz";
+        testSubject.setFile(filenameWithMultipleSuffixes);
+        const std::string expected = "file";
+        EXPECT_EQ(expected, testSubject.basename());
+}
 
 PRISM_END_TEST_NAMESPACE
 PRISM_END_NAMESPACE
