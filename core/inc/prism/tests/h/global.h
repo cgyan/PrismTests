a5c72ae2720563dcf7032537c488f739032f1651
iainhemstock <iainhemstock@googlemail.com>
2018-04-12 23:59:50 +0100

    Add UNIT_TEST and INTEGRATION_TEST macros

--- core/inc/prism/tests/h/global.h
+++ core/inc/prism/tests/h/global.h
@@ -3,8 +3,11 @@
 
 #include <prism/global>
 
+#define UNIT_TEST(TestClass) Unit_##TestClass
+#define INTEGRATION_TEST(TestClass) Integration_##TestClass
 #define GROUP_TEST_F(GroupName, TestBase, TestName) TEST_F(TestBase, GroupName##_##TestName)
-#define GROUP_TEST_P(GroupName, TestBase, TestName) TEST_P(TestBase, GroupName##_##TestName)
-#define GROUP_TEST(GroupName, TestBase, TestName) TEST(TestBase, GroupName##_##TestName)
+#define GROUP_TEST_P(GroupName, TestBase, TestName) TEST_P(GroupName##_##TestBase, TestName)
+#define GROUP_TEST(GroupName, TestBase, TestName) TEST(GroupName##_##TestBase, TestName)
+#define GROUP_INSTANTIATE_TEST_CASE_P(Prefix, TestCaseName, generator, ...) INSTANTIATE_TEST_CASE_P(Prefix, TestCaseName, generator)
 
 #endif
